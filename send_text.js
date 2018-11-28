8271f557caf76545deb5fd9c20dd073b47ac4796
arielamar123 <ariel.amar1@mail.huji.ac.il>
2018-11-28 18:39:58 +0200

    george push

--- send_text.js
+++ send_text.js
@@ -3,8 +3,30 @@ let socket = io.connect(LOCAL_HOST);
 
 
 function send_text(form) {
+    alert("shit");
     socket.emit('submit', form.header.value, form.footer.value);
+    console.log("mmmm")
 }
+
+socket.on("return_post", function (response) {
+    let post = document.createElement("textarea");
+    let output_headline = document.createElement("span");
+    let br = document.createElement("br");
+    let br2 = document.createElement("br");
+
+    output_headline.innerText = "Your post is:";
+    output_headline.style = "font-weight: bold;";
+    post.name = "post";
+    post.cols = "40";
+    post.rows = "5";
+    post.value = response;
+    $(".inputs").append(output_headline, br, br2, post);
+    // inputs = document.getElementsByName("inputs");
+    // inputs.appendChild(output_headline);
+    // inputs.appendChild(br);
+    // inputs.appendChild(post);
+
+});
 
 
 
